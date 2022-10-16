505c48ab6b580546d1a0f8c2189eda9aab2e7497
Summer-Lu0208 <slu12@chicagobooth.edu>
2022-10-16 17:28:41 +0000

    almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,56 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({:template=>"calculation_templates/square_form.html.erb"})
+
+  end
+
+  def calculate_square
+    @num=params.fetch("number").to_f
+
+    @square_of_num=@num * @num
+
+    render({:template=>"calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_square_root_form
+    render({:template=>"calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @num=params.fetch("number").to_f
+
+    @square_root_of_num=@num ** 0.5
+
+    render({:template=>"calculation_templates/square_root_results.html.erb"})
+  end
+
+  def blank_payment_form
+    render({:template=>"calculation_templates/payment_form.html.erb"})
+
+  end
+
+  def calculate_payment
+    @num1=params.fetch("user_apr").to_f
+    @num2=params.fetch("user_years").to_f
+    @num3=params.fetch("user_pv").to_f
+
+
+    render({:template=>"calculation_templates/payment_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({:template=>"calculation_templates/random_form.html.erb"})
+
+  end
+
+  def calculate_random
+    @num1=params.fetch("user_min").to_f
+    @num2=params.fetch("user_max").to_f
+    @result=rand(@num1..@num2)
+
+    render({:template=>"calculation_templates/random_results.html.erb"})
+  end
+
+
 end
